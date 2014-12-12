e6f19d82a934d2f571f4637aa0afa8c65c31ea10
Jared Nagle <jared@thenaglecode.com>
2014-12-12 11:00:30 +1100

    first working version

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,34 @@ var gulp = require('gulp'),
     stream = require('stream'),
     jshint = require('gulp-jshint'),
     shuffle = require('./index.js'),
+    del = require('del'),
     Readable = stream.Readable,
     PluginError = gutil.PluginError,
-    rs = new Readable();
+    rs = new Readable(),
+    paths = {
+        src: {
+            jshint: '*.js',
+            test: ['test/*.txt']
+        },
+        dest: {
+            test: '.tmp/output'
+        }
+    };
 
 gulp.task('jshint', function() {
-    return gulp.src('index.js')
+    return gulp.src(paths.src.jshint)
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('test', function() {
-    return gulp.src('test/*.txt')
-        .pipe(shuffle(/"use strict";[\n\s]*((?:\/\*[\s\S]*\*\/)[\n\s]*)[^\/]/g, /^/g, {verbose: true, captureGroup: 1}))
-        .pipe(gulp.dest('test/output'));
+    return gulp.src(paths.src.test)
+        .pipe(shuffle(/"use strict";[\n\s]*((?:\/\*[\s\S]*\*\/)[\n\s]*)[^\/]/g, /^/g, {captureGroup: 1}))
+        .pipe(gulp.dest(paths.dest.test));
+});
+
+gulp.task('clean', require('del').bind(null, ['.tmp']));
+
+gulp.task('default', ['clean'], function(){
+    return gulp.start('test');
 });
